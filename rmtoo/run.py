d8d2727133fc20e278d734d45255c8b6e8065632
Vytautas Astrauskas <vastrauskas@gmail.com>
2011-12-04 13:51:39 +0200

    Trying to solve problem with different main function calling.

--- rmtoo/run.py
+++ rmtoo/run.py
@@ -5,10 +5,12 @@ import sys
 from rmtoo.lib import RmtooMain
 
 
-def main(args):
+def main(args=None):
     """ Entry point for using with tools like buildout.
 
     Replacement for script ``bin/rmtoo``.
     """
+    if args is None:
+        args = sys.argv[1:]
 
     RmtooMain.main(args, sys.stdout, sys.stderr)
